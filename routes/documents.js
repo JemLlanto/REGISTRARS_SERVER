066f1cdd5a7c5fe9ae6ba31260578d0e2dafa1cd
JemLlanto <llantojem@gmail.com>
2025-02-25 08:42:20 +0800

    Update documents.js

--- routes/documents.js
+++ routes/documents.js
@@ -37,5 +37,65 @@ router.get("/fetchPurposes", (req, res) => {
     }
   });
 });
+router.get("/fetchYearGraduated", (req, res) => {
+  const query = "SELECT * FROM year_graduated";
+  db.query(query, (err, data) => {
+    if (err)
+      return res.json({
+        Status: "Error",
+        Message: "Error fetching purposes data.",
+      });
+    if (data.length > 0) {
+      return res.json({ Status: "Success", data: data });
+    } else {
+      return res.json({ Status: "Error", Message: "purposes not found" });
+    }
+  });
+});
+router.get("/fetchPurposeData", (req, res) => {
+  const { purposeName } = req.query;
+
+  if (!purposeName) {
+    return res.json({
+      Status: "Error",
+      Message: "Purpose name is required.",
+    });
+  }
+  const query = "SELECT * FROM purposes WHERE purposeName = ?";
+  db.query(query, [purposeName], (err, data) => {
+    if (err)
+      return res.json({
+        Status: "Error",
+        Message: "Error fetching selections data.",
+      });
+    if (data.length > 0) {
+      return res.json({ Status: "Success", data: data[0] });
+    } else {
+      return res.json({ Status: "Error", Message: "selections not found" });
+    }
+  });
+});
+router.get("/fetchSelections", (req, res) => {
+  const { purposeID } = req.query;
+  if (!purposeID) {
+    return res.json({
+      Status: "Error",
+      Message: "Purpose ID is required",
+    });
+  }
+  const query = "SELECT * FROM purpose_selection WHERE purposeID = ?";
+  db.query(query, [purposeID], (err, data) => {
+    if (err)
+      return res.json({
+        Status: "Error",
+        Message: "Error fetching selections data.",
+      });
+    if (data.length > 0) {
+      return res.json({ Status: "Success", data: data });
+    } else {
+      return res.json({ Status: "Error", Message: "selections not found" });
+    }
+  });
+});
 
 export default router;
